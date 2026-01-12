828d7488570846dd99543eb09620eed668a05945
kapKurgan <kkaapp@yandex.ru>
2026-01-12 21:17:11 +0500

    Переименовать класс Locators в SitePages

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 from playwright.sync_api import Page
 from pages.site_pages import BASE_URL, SitePages, LOGO_PAGE_HOME
-from utils.checks import open_page, expect_url, expect_visible, expect_text
+from utils.checks import open_page, check_url, check_text, check_locator
 
 
 @allure.title("Подготовка тестового окружение (фикстура)")
@@ -14,8 +14,8 @@ def open_home_page(page: Page) -> SitePages:
     """
     print(f"\n⚙️ Подготовительные действия для теста")
     open_page(page, BASE_URL)
-    expect_url(page, BASE_URL)
+    check_url(page, BASE_URL)
     login_page = SitePages(page)
-    expect_visible(login_page.logo_url, LOGO_PAGE_HOME)
-    expect_text(login_page.logo_url, LOGO_PAGE_HOME)
+    check_locator(login_page.logo_url, LOGO_PAGE_HOME, page)
+    check_text(login_page.logo_url, LOGO_PAGE_HOME, page)
     return login_page
